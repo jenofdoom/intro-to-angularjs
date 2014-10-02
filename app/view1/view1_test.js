9bcf55f85b0db87e4d2b055960b7bc7a0d8cfa73
jenofdoom <j_zajac@email.com>
2014-10-02 18:25:25 +1300

    Unit test with a mock

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -6,16 +6,28 @@ describe('myApp.view1 module', function() {
 
   describe('view1 controller', function(){
 
-    var scope, ctrl;
-    beforeEach(inject(function($rootScope, $controller) {
+    var $httpBackend, scope, ctrl;
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+        $httpBackend = _$httpBackend_;
+        $httpBackend.expectGET('articles.json').
+            respond([
+               {title: 'Test Article 1'},
+               {title: 'Test Article 2'}
+            ]);
         scope = $rootScope.$new();
         ctrl = $controller('View1Ctrl', {$scope: scope});
     }));
 
-
     it('should have a scope', inject(function() {
-      expect(ctrl).toBeDefined();
+        expect(ctrl).toBeDefined();
     }));
+
+    it('should create an "articles" model from xhr data', inject(function() {
+        expect(scope.articles.length).toBe(0);
+        $httpBackend.flush();
+        expect(scope.articles.length).toBe(2);
+    }));
+
 
   });
 });
