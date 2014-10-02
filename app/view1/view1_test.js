0bc74bb7e53616ff0e0c559dd3e7553727ccd287
jenofdoom <j_zajac@email.com>
2014-10-02 18:11:17 +1300

    Fixing the other unit test

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -2,14 +2,19 @@
 
 describe('myApp.view1 module', function() {
 
-  beforeEach(module('myApp.view1'));
+  beforeEach(module('myApp.view1', 'myApp.articles', 'ngResource'));
 
   describe('view1 controller', function(){
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
+    var scope, ctrl;
+    beforeEach(inject(function($rootScope, $controller) {
+        scope = $rootScope.$new();
+        ctrl = $controller('View1Ctrl', {$scope: scope});
+    }));
+
+
+    it('should have a scope', inject(function() {
+      expect(ctrl).toBeDefined();
     }));
 
   });
